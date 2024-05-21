5dda0963cf43c014851ddc6fac40b38b7ba115fa
AlejoZA <117043041+AlejoZA@users.noreply.github.com>
2024-05-21 18:51:53 -0500

    Update backends.py

--- users/backends.py
+++ users/backends.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
-
 class EmailOrUsernameModelBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         if "@" in username:
