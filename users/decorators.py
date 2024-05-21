6b635a2c1b72d86d638c81f58a75dae38f098d80
AlejoZA <117043041+AlejoZA@users.noreply.github.com>
2024-05-21 18:52:08 -0500

    Update decorators.py

--- users/decorators.py
+++ users/decorators.py
@@ -1,7 +1,6 @@
 from django.shortcuts import redirect
 from functools import wraps
 
-
 def redirect_authenticated_user(view, redirect_to='users:home'):
     @wraps(view)
     def wrapper(request, *args, **kwargs):
